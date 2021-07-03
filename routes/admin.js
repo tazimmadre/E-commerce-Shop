22d28bd3f309470c33dc0683289537998835d329
Tazim_Git <tazimmadre5041@gmail.com>
2021-07-03 09:12:21 +0530

    3 Junly 2021

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const adminController=require('../controllers/admin');
+
+const router = express.Router();
+
+
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products
+router.get('/products');
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+module.exports=router;
